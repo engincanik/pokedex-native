918941e96c773ea02f737ee7904b731b0ba97894
engincanik <engincanik@gmail.com>
2022-02-20 15:48:53 +0300

    :sparkles: feat: Add bottom navigation and mock search function

--- app/src/main/java/com/engin/pokedex/view/MainActivity.kt
+++ app/src/main/java/com/engin/pokedex/view/MainActivity.kt
@@ -3,12 +3,19 @@ package com.engin.pokedex.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.engin.pokedex.R
+import com.engin.pokedex.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.bottomNavigationView.background = null
+        binding.bottomNavigationView.menu.getItem(1).isEnabled = false
     }
 }
